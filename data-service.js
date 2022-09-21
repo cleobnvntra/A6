862e4456d3303ab5b52b22f60ef5023e17e5fc10
cleobnvntra <97420565+cleobnvntra@users.noreply.github.com>
2022-09-21 14:08:13 -0400

    update-server

--- data-service.js
+++ data-service.js
@@ -1,12 +1,28 @@
 var employees = [];
 var departments = [];
+var fs = require('fs');
 
-function initialize() {
+module.exports.initialize = function() {
     
     return new Promise(function(resolve, reject){
         fs.readFile('./data/employees.json',(err,data)=>{
             if (err) reject("Failure to read file employees.json!");
             employees = JSON.parse(data);
         });
+
+        fs.readFile('./data/departments.json',(err,data)=>{
+            if (err) reject("Failure to read file departments.json!");
+            departments = JSON.parse(data);
+        });
+
+        resolve();
+    });
+}
+
+module.exports.getAllEmployees = function() {
+
+    return new Promise(function(resolve, reject) {
+        if(employees.length == 0) reject("No results returned.");
+        resolve();
     });
 }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var data_service = require("./data-service.js")
+var data_service = require("./data-service.js");
 var path = require("path");
 var app = express();
 
@@ -9,16 +9,6 @@ var HTTP_PORT = process.env.PORT || 8080;
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
-
-// // setup a 'route' to listen on the default url path (http://localhost)
-// app.get("/", function(req,res){
-//     res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
-// });
-
-// // setup another route to listen on /about
-// app.get("/about", function(req,res){
-//     res.send("<h3>asdsadsadsa</h3>");
-// });
 
 app.use(express.static('public'));
 
@@ -35,12 +25,17 @@ app.get("/managers", function(req,res){
 })
 
 app.get("/employees", function(req,res){
-    
-})
+  data_service.getAllEmployees().then(function(data) {
+    res.json(data);
+  })
+  .catch(function(err){
+    res.send(err);
+  });
+});
 
 app.get("/departments", function(req,res){
     
 })
 
 // setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+app.listen(HTTP_PORT, data_service.initialize().then(function());
