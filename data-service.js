22b36f1d5d4ef5c51ab9a0acebede4c5fb2e1303
cleobnvntra <97420565+cleobnvntra@users.noreply.github.com>
2022-10-04 20:54:43 -0400

    A3 server update

--- data-service.js
+++ data-service.js
@@ -3,6 +3,7 @@ var departments = [];
 var managers = [];
 var fs = require('fs');
 
+/**reads departments and employees json files to store in the array*/
 module.exports.initialize = function() {
     
     return new Promise(function(resolve, reject){
@@ -27,35 +28,113 @@ module.exports.initialize = function() {
     });
 }
 
+/**returns a promise to list of all employees*/
 module.exports.getAllEmployees = function() {
 
     return new Promise(function(resolve, reject) {
-        if(employees.length == 0) reject("No results returned.");
+        if(employees.length == 0) reject("No employees were found.");
         resolve(employees);
     });
 }
 
+/**returns a promise to list of managers.
+ * uses aManager() function.*/
 module.exports.getManagers = function() {
 
     return new Promise(function(resolve, reject) {
         employees.forEach(aManager);
 
-        if (managers.length == 0) reject("No results returned.");
+        if (managers.length == 0) reject("No employees were found.");
         resolve(managers);
         managers = [];
     });
 }
 
+/**returns a promise to list of departments*/
 module.exports.getDepartments = function() {
 
     return new Promise(function(resolve, reject) {
-        if(departments.length == 0) reject("No results returned.");
+        if(departments.length == 0) reject("No employees were found.");
         resolve(departments);
     });
 }
 
-function aManager(item) {
-    if(item.isManager == true) {
-        managers.push(item);
+/**returns a promise to add employee data to array using form*/
+module.exports.addEmployee = function(employeeData) {
+
+    return new Promise(function(resolve, reject) {
+        if (employeeData.firstName == "" || employeeData.lastName == "") {
+            reject("Invalid data.");
+        }
+        else {
+            if(typeof employeeData.isManager === 'undefined' ) {
+                employeeData.isManager = false;
+            }
+
+            employeeData.employeeNum = employees.length + 1;
+            employees.push(employeeData);
+            resolve;
+        }
+    });
+}
+
+/**function call for status query string*/
+module.exports.getEmployeesByStatus = function(status) {
+    return new Promise(function(resolve, reject) {
+        var empByStat = [];
+        for(var i = 0; i < employees.length; ++i) {
+            if(status == employees[i].status) {
+                empByStat.push(employees[i]);
+            }
+        }
+        if(empByStat.length == 0) reject("No employees were found.");
+        resolve(empByStat);
+    });
+}
+
+/**function call for department query string*/
+module.exports.getEmployeesByDepartment = function(department) {
+    return new Promise(function(resolve, reject) {
+        var empByDep = [];
+        for(var i = 0; i < employees.length; ++i) {
+            if(department == employees[i].department) {
+                empByDep.push(employees[i]);
+            }
+        }
+        if(empByDep.length == 0) reject("No employees were found.");
+        resolve(empByDep);
+    });
+}
+
+/**function call for manager query string*/
+module.exports.getEmployeesByManager = function(manager) {
+    return new Promise(function(resolve, reject) {
+        var empByManager = [];
+        for(var i = 0; i < employees.length; ++i) {
+            if(manager == employees[i].employeeManagerNum) {
+                empByManager.push(employees[i]);
+            }
+        }
+        if(empByManager.length == 0) reject("No employees were found.");
+        resolve(empByManager);
+    })
+}
+
+/**function call for one parameter route*/
+module.exports.getEmployeesByNum = function(empNum) {
+    return new Promise(function(resolve, reject) {
+        for(var i = 0; i < employees.length; ++i) {
+            if(empNum == employees[i].employeeNum) {
+                resolve(employees[i]);
+            }
+        }
+        reject("No employee found.");
+    })
+}
+
+/**pushes employee data if employee is a manager */
+function aManager(emp) {
+    if(emp.isManager == true) {
+        managers.push(emp);
     }
 }
--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 /*************************************************************************
-* BTI325– Assignment 2
+* BTI325– Assignment 3
 * I declare that this assignment is my own work in accordance with Seneca Academic
 Policy. No part * of this assignment has been copied manually or electronically from any
 other source
 * (including 3rd party web sites) or distributed to other students.
 *
-* Name: Cleo Buenaventura Student ID: 030626139 Date: 9/27/2022
+* Name: Cleo Buenaventura Student ID: 030626139 Date: 10/4/2022
 *
-* Your app’s URL (from Cyclic) : https://burgundy-worm-tam.cyclic.app
+* Your app’s URL (from Cyclic) : 
 *
 *************************************************************************/ 
 
 var express = require("express");
 var data_service = require("./data-service.js");
 var path = require("path");
+var multer = require("multer");
 var app = express();
+var fs = require("fs");
 
 var HTTP_PORT = process.env.PORT || 8080;
 
@@ -23,53 +25,146 @@ function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const storage = multer.diskStorage({
+  destination: "./public/images/uploaded",
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage: storage });
+
 app.use(express.static('public'));
 
-app.get("/about", function(req,res) {
-    res.sendFile(path.join(__dirname,"/views/about.html"));
-});
-
+//HOME ROUTE
+//HOME PAGE
 app.get("/", function(req,res) {
-    res.sendFile(path.join(__dirname,"/views/home.html"));
+  res.sendFile(path.join(__dirname,"/views/home.html"));
 });
 
+//ABOUT ROUTE
+//ABOUT PAGE
+app.get("/about", function(req,res) {
+  res.sendFile(path.join(__dirname,"/views/about.html"));
+});
+
+//MANAGERS ROUTE
+//LIST OF MANAGERS
 app.get("/managers", function(req,res) {
   data_service.getManagers().then(function(data) {
     res.json(data);
   })
   .catch(function(err) {
-    res.json(err);
+    res.send(err);
   });
 });
 
+//EMPLOYEE ROUTE
+//LIST OF ALL EMPLOYEES
 app.get("/employees", function(req,res) {
-  data_service.getAllEmployees().then(function(data) {
+  if(req.query.status) {
+    data_service.getEmployeesByStatus(req.query.status).then(function(data) {
+      res.json(data);
+    }).catch(function(err) {
+      res.send(err);
+    });
+  }
+  else if(req.query.department) {
+    data_service.getEmployeesByDepartment(req.query.department).then(function(data) {
+      res.json(data);
+    }).catch(function(err) {
+      res.send(err);
+    });
+  }
+  else if(req.query.manager) {
+    data_service.getEmployeesByManager(req.query.manager).then(function(data) {
+      res.json(data);
+    }).catch(function(err) {
+      res.send(err);
+    });
+  }
+  else {
+    data_service.getAllEmployees().then(function(data) {
+        res.json(data);
+      }
+    ).catch(function(err) {
+      res.send(err);
+    });
+  }
+});
+
+//SINGLE EMPLOYEE ROUTE
+//DISPLAY EMPLOYEE BY EMPLOYEENUM
+app.get("/employee/:employeeNum", function(req,res) {
+  data_service.getEmployeesByNum(req.params.employeeNum).then(function(data) {
     res.json(data);
-  })
-  .catch(function(err) {
-    res.json(err);
+  }).catch(function(err){
+    res.send(err);
   });
 });
 
+//DEPARTMENT ROUTE
+//LIST OF DEPARTMENTS
 app.get("/departments", function(req,res) {
     data_service.getDepartments().then(function(data) {
       res.json(data);
     })
     .catch(function(err) {
-      res.json(err);
+      res.send(err);
     });
 });
 
+//ADD EMPLOYEE ROUTE
+//ADD EMPLOYEE FORM
+app.get("/employees/add", function(req,res) {
+  res.sendFile(path.join(__dirname,"/views/addEmployee.html"));
+});
+
+//ADD EMPLOYEE POST
+//ADDS EMPLOYEE DATA
+app.post("/employees/add", function(req,res) {
+  data_service.addEmployee(req.body).then(function(data) {
+    res.redirect("/employees");
+  }).catch(function(err) {
+    res.send(err);
+  });
+});
+
+//ADD IMAGE ROUTE
+//ADD IMAGE FORM
+app.get("/images/add", function(req,res) {
+  res.sendFile(path.join(__dirname,"/views/addImage.html"));
+});
+
+//IMAGE ROUTE
+//LIST OF IMAGES
+app.get("/images", function(req,res) {
+  fs.readdir("./public/images/uploaded", function(err, images){
+    if(!err) {
+      res.json({images});
+    }
+  });
+});
+
+//ADD IMAGE ROUTE
+//UPLOAD IMAGE
+app.post("/images/add", upload.single("imageFile"), function(req,res) {
+  res.redirect("/images");
+});
+
+//ERROR ROUTE
+//ERROR PAGE
 app.get("*", function(req,res) {
   res.sendFile(path.join(__dirname,"/views/error.html"))
-})
+});
 
 // setup http server to listen on HTTP_PORT
 data_service.initialize()
 .then(function (response) {
   app.listen(HTTP_PORT, onHttpStart);
   console.log(response);
-})
-.catch(function (err) {
+}).catch(function (err) {
   console.log(err);
 });
